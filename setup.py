6567391e1065da3dec0b7bdaf14fc7d5fb1bb7a0
Camden Richter <richtercamden@gmail.com>
2017-05-03 14:25:43 -0400

    Major updates

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Game',
+    version='0.1.5',
+    packages=[''],
+    url='richtercamden.github.io/docs/wrote/index.html',
+    license='',
+    author='Camden Richter',
+    author_email='richtercamden@gmail.com',
+    description=''
+)
